db5640621d6f0a4eb15b591cb3dec5f47c394c11
Mirek <nyziu@DESKTOP-N9P8O81>
2023-03-11 17:44:19 -0500

    Delete route for api/notes

--- server.js
+++ server.js
@@ -28,14 +28,15 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
-
+// API Routes
+// Get route for api notes
 app.get('/api/notes', (req, res) => {
     readFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
 
-
+// POST route for notes
 app.post('/api/notes', (req, res) => {
-    console.log(req.body)    
+    console.log(req.body)
 
     const { title, text } = req.body;
 
@@ -44,29 +45,57 @@ app.post('/api/notes', (req, res) => {
             title,
             text,
             // Add unique id using uuid npm
-            note_id: uuidv4()
-        };        
-        
+            id: uuidv4().slice(0, 8)
+        };
+
         readFile('./db/db.json', 'utf-8').then((data) => {
-            
+
             // Create object from jason file
             const parsedData = JSON.parse(data);
-            
+            // Push new note into array of notes
             parsedData.push(newNote);
-
-            // Write note to db file
+            // Write updated notes object to db file as string
             fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) =>
-                err ? console.error(err) : console.info(`\nData written to database.`)
-            );                       
-
+                err ? console.error(err) : console.info(`\nData written to db.json file.`)
+            );
+            // Complete request by sending successful response 
             res.status(200).json("Note added successfully");
-        }) 
+        })
     } else {
-        res.json("Failed to add note.")
+        // Complete request not
+        res.status(400).json("Failed to add note.")
     }
 })
 
+// DELETE route for api notes
+app.delete('/api/notes/:id', (req, res) => {
+    // Assign ID passed as query parameter to variable
+    const idToDelete = req.params.id;
+    console.log("🚀 ~ file: server.js:74 ~ app.delete ~ idToDelete:", idToDelete)
 
+    if (idToDelete) {
+        readFile('./db/db.json', 'utf-8').then((data) => {
+            // Create object from jason file
+            const parsedData = JSON.parse(data);
+
+            parsedData.forEach((element, index) => {
+                if (element.id === idToDelete) {
+
+                    parsedData.splice(index, 1);
+
+                    fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) =>
+                        err ? console.error(err) : console.info(`\nData written to db.json file.`)
+                    );
+                    // Complete request by sending successful response 
+                    res.status(200).json("Note deleted successfully");
+                }
+            });
+        });
+    }     
+        
+    res.status(400).json("Note id could not be found.");
+    
+})
 
 
 
@@ -80,6 +109,6 @@ app.get('*', (req, res) => {
 
 
 
-app.listen(PORT, () => 
+app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
 );
